032d0525f7b6a800ba2d9abcbdd810335ae83a23
nier <17875512076@163.com>
2018-08-30 14:33:20 +0800

    bean

---
+++ OurBookingV1/src/main/java/com/king/Booking/bean/SearchHotelList.java
@@ -0,0 +1,110 @@
+package com.king.Booking.bean;
+/**
+ * 搜索酒店Bean类
+ * @author Nier
+ *
+ */
+public class SearchHotelList {
+	private int hotelId;		//酒店ID
+	private double roomMin;		//房间最低价
+	private int roomRating;		//住宿评级
+	private String leisureEntertainment;	//休闲乐活
+	private int roomQuantity;	//住宿房量
+	private int discount;		//折扣优惠
+	private int frontDesk;		//24小时前台
+	private int cancelPrepay;	//取消和预付
+	private int mealDinner;		//餐点
+	private int roomType;		//住处类型
+	private String landMark;	//地标
+	private int rating;			//评分
+	private String chainHotels;	//连锁酒店
+	private int searchHotelId;	//搜索酒店ID
+	public int getHotelId() {
+		return hotelId;
+	}
+	public void setHotelId(int hotelId) {
+		this.hotelId = hotelId;
+	}
+	public double getRoomMin() {
+		return roomMin;
+	}
+	public void setRoomMin(double roomMin) {
+		this.roomMin = roomMin;
+	}
+	public int getRoomRating() {
+		return roomRating;
+	}
+	public void setRoomRating(int roomRating) {
+		this.roomRating = roomRating;
+	}
+	public String getLeisureEntertainment() {
+		return leisureEntertainment;
+	}
+	public void setLeisureEntertainment(String leisureEntertainment) {
+		this.leisureEntertainment = leisureEntertainment;
+	}
+	public int getRoomQuantity() {
+		return roomQuantity;
+	}
+	public void setRoomQuantity(int roomQuantity) {
+		this.roomQuantity = roomQuantity;
+	}
+	public int getDiscount() {
+		return discount;
+	}
+	public void setDiscount(int discount) {
+		this.discount = discount;
+	}
+	public int getFrontDesk() {
+		return frontDesk;
+	}
+	public void setFrontDesk(int frontDesk) {
+		this.frontDesk = frontDesk;
+	}
+	public int getCancelPrepay() {
+		return cancelPrepay;
+	}
+	public void setCancelPrepay(int cancelPrepay) {
+		this.cancelPrepay = cancelPrepay;
+	}
+	public int getMealDinner() {
+		return mealDinner;
+	}
+	public void setMealDinner(int mealDinner) {
+		this.mealDinner = mealDinner;
+	}
+	public int getRoomType() {
+		return roomType;
+	}
+	public void setRoomType(int roomType) {
+		this.roomType = roomType;
+	}
+	public String getLandMark() {
+		return landMark;
+	}
+	public void setLandMark(String landMark) {
+		this.landMark = landMark;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public String getChainHotels() {
+		return chainHotels;
+	}
+	public void setChainHotels(String chainHotels) {
+		this.chainHotels = chainHotels;
+	}
+	public int getSearchHotelId() {
+		return searchHotelId;
+	}
+	public void setSearchHotelId(int searchHotelId) {
+		this.searchHotelId = searchHotelId;
+	}
+	
+	
+	
+	
+}
