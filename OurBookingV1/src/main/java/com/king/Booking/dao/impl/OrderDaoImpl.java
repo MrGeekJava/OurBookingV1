92a857347ed9b7d20da5555f84c6c19af0dfc52c
nier <17875512076@163.com>
2018-09-04 09:48:07 +0800

    OrderDao

---
+++ OurBookingV1/src/main/java/com/king/Booking/dao/impl/OrderDaoImpl.java
@@ -0,0 +1,28 @@
+package com.king.Booking.dao.impl;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+
+import com.king.Booking.dao.interfaces.OrderDao;
+import com.king.Booking.entity.OrderView;
+import com.king.Booking.util.DataSourceUtil;
+
+public class OrderDaoImpl implements OrderDao{
+	
+//	查询所有该用户的订单根据订单号来查询
+	public List<OrderView> queryAllOrderbyOrderId(String OrderId) throws SQLException {
+		QueryRunner runner = new QueryRunner();
+		Connection conn = DataSourceUtil.getConnection();
+		String sql = "select * from order_view where OrderId=?";
+		Object[] params = {OrderId};
+		List<OrderView> mapOrders = new ArrayList<OrderView>();
+		mapOrders = runner.query(conn, sql, new BeanListHandler<OrderView>(OrderView.class), params);
+		return mapOrders;
+	}
+	
+}
